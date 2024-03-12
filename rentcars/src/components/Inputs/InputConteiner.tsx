1664d8ef6478fec6ea5b8a4fbaa3ad1b4433720b
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-03-12 00:31:49 -0300

    Button

--- rentcars/src/components/Inputs/InputConteiner.tsx
+++ rentcars/src/components/Inputs/InputConteiner.tsx
@@ -4,21 +4,23 @@ const InputContainer = () => {
   return (
     <div style={{ 
       backgroundColor: '#fff', 
-      padding: '30px', 
+      padding: '20px', // Reduzindo o padding para diminuir o tamanho do container
       borderRadius: '10px', 
-      marginTop: '350px',
+      marginTop: '50px', 
       boxShadow: 'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px',
       display: 'flex', 
       flexDirection: 'column', 
       alignItems: 'center',
+      width: '80%', // Definindo uma largura fixa para o container
+      maxWidth: '900px', // Limitando a largura máxima para manter o conteúdo legível
+      margin: '0 auto', // Centralizando o container horizontalmente
     }}>
       <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', marginBottom: '20px' }}>
-        {/* Inputs com largura relativa */}
         <input 
           type="text" 
           placeholder="Local" 
           style={{ 
-            flex: '1', // Usando flex para o input crescer horizontalmente
+            flex: '2', 
             height: '40px', 
             padding: '10px', 
             fontSize: '16px', 
@@ -51,12 +53,11 @@ const InputContainer = () => {
           }} 
         />
       </div>
-      {/* Botão abaixo dos inputs */}
       <button 
         style={{
           width: '300px',
           height: '50px',
-          backgroundColor: '#4CAF50',
+          backgroundColor: '#113259',
           color: 'white',
           border: 'none',
           borderRadius: '5px',
