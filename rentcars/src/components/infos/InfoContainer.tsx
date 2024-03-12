11c3eb05965c3f7ad7689d6305ff3643b559a5ad
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-03-12 20:36:35 -0300

    InfoContainer

--- rentcars/src/components/infos/InfoContainer.tsx
+++ rentcars/src/components/infos/InfoContainer.tsx
@@ -1,10 +1,13 @@
+import textImage from '../../assets/text.png'; // Importe a imagem
+import BookButton from '../button/BookButton';
 
 const InfoContainer = () => {
   return (
     <div>
-        <h6>-Welcome</h6>
-        <h1>Rent the best quality car's</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est iste ea pariatur odit modi excepturi accusamus. Quaerat, ullam! Impedit cupiditate aliquam voluptatibus, ipsam similique voluptatum ipsa nobis enim non corporis.</p>
+      <div>
+        <img src={textImage} alt="Text" />
+      </div>
+      <BookButton></BookButton>
     </div>
   );
 }
