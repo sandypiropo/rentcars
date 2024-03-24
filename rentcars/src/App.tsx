04df531775ac8d7ee90a72e4e49a63e12a943f84
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-03-24 02:39:33 -0300

    Pages - New App.tsx

--- rentcars/src/App.tsx
+++ rentcars/src/App.tsx
@@ -1,40 +1,10 @@
-import './App.css'
-import Navbar from './components/navbar/NavBar'
-import DoubleImg from './components/content/ImageSection'
-import Button from './components/button/Button'
-import Inputs from './utils/InputContainer'
-import Footer from './components/footer/Footer'
+import './App.css';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div>
-      <div className='body'>
-        <div>
-          <Navbar/> 
-          <div className='content'>
-            <div className='div1'>
-              <h3 className='h3'>Discover the world </h3>
-                <h1 className='h1'>Save. Compare. Rent.</h1>
-                <p className='p'>Compare the best prices from over 300 car rental companies <br/> 
-                  When you rent with Rentcars, you're sure to get a great car at an affordable price.</p>
-              <div className='btn1'>
-                <Button onClick={() => console.log("Button clicked")} />
-              </div>
-            </div>
-          
-            <div className='div2'>
-              <DoubleImg/>
-            </div>
-    
-            <div className='div3'>
-              <Inputs/>
-            </div>
-          </div>  
-        </div>
-          <div className='div4'>
-            <Footer/>
-          </div>
-      </div>
+      <Home/>
     </div>
   )
 }
